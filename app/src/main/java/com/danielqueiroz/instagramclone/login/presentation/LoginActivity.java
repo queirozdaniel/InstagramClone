d87767097149ce9fcc70f32c18f2bed7b95903b8
queirozdaniel <danielqueirozak77@gmail.com>
2020-09-20 13:58:03 -0300

    Adicionando elemento UI de erro em formulário

--- app/src/main/java/com/danielqueiroz/instagramclone/login/presentation/LoginActivity.java
+++ app/src/main/java/com/danielqueiroz/instagramclone/login/presentation/LoginActivity.java
@@ -1,10 +1,14 @@
 package com.danielqueiroz.instagramclone.login.presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.danielqueiroz.instagramclone.R;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,5 +16,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextInputLayout inputLayout = findViewById(R.id.login_edit_text_email_input);
+        inputLayout.setError("Esse email é inválido");
+
+        EditText editText = findViewById(R.id.login_edit_text_email);
+        editText.setBackground(ContextCompat.getDrawable(LoginActivity.this, R.drawable.edit_text_background_error));
+
     }
 }
