a684ee82770414878e405cdecacd3a204c816fa2
queirozdaniel <danielqueirozak77@gmail.com>
2020-09-22 21:59:13 -0300

    Criando Dialog customizado

--- app/src/main/java/com/danielqueiroz/instagramclone/register/presentation/RegisterPhotoFragment.java
+++ app/src/main/java/com/danielqueiroz/instagramclone/register/presentation/RegisterPhotoFragment.java
@@ -10,15 +10,29 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.danielqueiroz.instagramclone.R;
+import com.danielqueiroz.instagramclone.common.view.CustomDialog;
 
 public class RegisterPhotoFragment extends Fragment {
 
-    public RegisterPhotoFragment() { }
+    public RegisterPhotoFragment() {
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //TODO: scroll gravity top
         return inflater.inflate(R.layout.fragment_register_photo, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        CustomDialog customDialog = new CustomDialog.Builder(getContext())
+                .setTitle(R.string.define_photo_profile)
+                .addButton((v) -> {
+                }, R.string.search_gallery)
+                .build();
+
+        customDialog.show();
     }
 }
